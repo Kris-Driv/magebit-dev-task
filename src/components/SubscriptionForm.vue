af706b06cfdf99a52667a66c75e70bd75c71efb4
Kristaps <K.Drivnieks@gmail.com>
2021-01-09 04:55:38 +0200

    frontend done

--- src/components/SubscriptionForm.vue
+++ src/components/SubscriptionForm.vue
@@ -1,37 +1,62 @@
 <template>
     <div class="box-wrapper">
-        <h1>Subscribe to newsletter</h1>
-        <p class="box-wrapper__sub-heading">Subscribe to our newsletter and get 10% discount on pineapple glasses.</p>
-        <form action="subscribe.php" method="POST"></form>
-        <div class="custom-input">
-            <input 
-                type="email" 
-                class="custom-input__text" 
-                placeholder="Type your email address here..."
-                v-model="email"
-                v-on:keydown="validate"
-                >
-            <a href="#" class="custom-input__submit" v-on:click="submitForm"></a>
+        <div v-if="subscribed === false">
+            <h1>Subscribe to newsletter</h1>
+            <p class="box-wrapper__sub-heading">Subscribe to our newsletter and get 10% discount on pineapple glasses.</p>
+            <form action="subscribe.php" method="POST"></form>
+            <div class="custom-input">
+                <input 
+                    type="email" 
+                    class="custom-input__text" 
+                    placeholder="Type your email address here..."
+                    v-model="email"
+                    @keyup="validate"
+                    >
+                <a href="#" class="custom-input__submit" v-on:click="submitForm"></a>
+                <span class="error-message" v-if="error.length > 0">{{ error }}</span>
+            </div>
+            <div class="tos">
+                <input class="tos__checkbox" type="checkbox" v-model="tos" @change="clicked = true; validate();">
+                <span class="tos__text">I agree to <span class="tos__underline">terms of service</span></span>
+            </div>
+            <div class="socials">
+                <ul class="socials-list">
+                    <li>
+                        <SocialBadge name="facebook"/>
+                    </li>
+                    <li>
+                        <SocialBadge name="instagram"/>
+                    </li>
+                    <li>
+                        <SocialBadge name="twitter"/>
+                    </li>
+                    <li>
+                        <SocialBadge name="youtube"/>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="tos">
-            <input class="tos__checkbox" type="checkbox">
-            <span class="tos__text">I agree to <span class="tos__underline">terms of service</span></span>
-        </div>
-        <div class="socials">
-            <ul class="socials-list">
-                <li>
-                    <SocialBadge name="facebook"/>
-                </li>
-                <li>
-                    <SocialBadge name="instagram"/>
-                </li>
-                <li>
-                    <SocialBadge name="twitter"/>
-                </li>
-                <li>
-                    <SocialBadge name="youtube"/>
-                </li>
-            </ul>
+        <div v-else>
+            <img src="../assets/images/ic_success.svg" class="success-logo">
+            <h1>Thanks for subscribing!</h1>
+            <p class="box-wrapper__sub-heading">You have successfully subscribed to our email listing. Check your email for the discount code.</p>
+            <div class="spacer"></div>
+            <div class="socials">
+                <ul class="socials-list">
+                    <li>
+                        <SocialBadge name="facebook"/>
+                    </li>
+                    <li>
+                        <SocialBadge name="instagram"/>
+                    </li>
+                    <li>
+                        <SocialBadge name="twitter"/>
+                    </li>
+                    <li>
+                        <SocialBadge name="youtube"/>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -47,14 +72,21 @@ export default {
     data: function() {
         return {
             email: "",
-            validated: false,
+            subscribed: false,
+            error: "",
+            clicked: false,
+            tos: false
         }
     },
     methods: {
-        submitForm: function() {
+        submitForm: function(e) {
+            e.preventDefault();
+            console.log(this.tos);
+
+            this.clicked = true;
             this.validate();
 
-            // if(this.validated) {
+            if(this.error.length < 1) {
                 this.$http.get('http://localhost:8000/insert', {
                     params: {
                         email: this.email
@@ -62,14 +94,33 @@ export default {
                 }).then(response => {
                     // redirect
                     console.log(response);
+                    if(response.status === 200) {
+                        this.subscribed = true;
+                    }
                 }, err => {
-                    console.error(err);
+                    if(err.response && err.response.data && err.response.data.message) {
+                        let message = err.response.data.message;
+                        this.error = message.charAt(0).toUpperCase() + message.slice(1);
+                    }
                 });
-            // }
+            }
         },
         validate: function() {
-            this.validated = false;
-        }
+            if(!this.clicked) return;
+
+            if(this.email.length < 1) {
+                this.error = "Email address is required";
+            } else if(!this.validateEmail(this.email)) {
+                this.error = "Please provide a valid e-mail address";  
+            } else if(this.tos === false) {
+                this.error = "You must accept the terms and conditions";
+            } else if(this.email.substr(this.email.length - 2, 2).toLowerCase() === "co") {
+                this.error = "We are not accepting subscriptions from Colombia emails";
+            } else {
+                this.error = "";
+                this.clicked = false;
+            }
+        },
     },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 Vue.use(VueAxios, axios);
 
+Vue.mixin({
+  methods: {
+    validateEmail: function(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
+  },
+})
+
 Vue.config.productionTip = false
 
 new Vue({
